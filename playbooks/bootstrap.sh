f9fdd0a91cf6cfe70856c115f8b564b4ad437e86
Bill Knight <billknig@gmail.com>
2025-06-09 09:58:44 -0400

    updated bootstrap process

---
+++ playbooks/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update package manager and install Ansible
+sudo apt update
+sudo apt install -y ansible
+sudo apt install -y openssh-server
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.original
+sudo chmod a-w /etc/ssh/sshd_config.original
+
+# Download the Ansible playbook
+curl -O http://192.168.86.83/bootstrap.yml
+
+# Run the Ansible playbook using ansible-playbook command
+ansible-playbook -i localhost bootstrap.yml
